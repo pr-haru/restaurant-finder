ad150b1694cae32d2db9f86dc28e57df6fdd49a6
pr-haru <ec2-user@ip-10-0-10-91.ap-northeast-1.compute.internal>
2025-11-13 13:20:53 +0000

    こみっと

--- Infrastructure/api/GetRequest.go
+++ Infrastructure/api/GetRequest.go
@@ -20,38 +20,20 @@ type OpenAIGenerator struct {
 
 
 func (g *OpenAIGenerator) GenerateSearchQuery(prompt string) (*entity.HotPepperRequestParams, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
-	s3Client := s3.NewFromConfig(cfg)
+filepath = ../../C:format.json
+// data, err := os.ReadFile(filePath)
+// 	if err != nil {
+// 		fmt.Printf("Error reading file: %v\n", err)
+// 		return
+// 	}
 
-	bucketName := "hotpepper-api-definitions" 
-	objectKey := "format.json" 
-
-	// --- 4. S3からファイル（オブジェクト）を取得 ---
-	// GetObjectInputに必要な情報を詰めてAPIを呼び出します。
-	resp, err := s3Client.GetObject(context.TODO(), &s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
-	})
-	if err != nil {
-		log.Fatalf("failed to get object, %v", err)
-	}
-
-	// 関数を抜ける際に必ずBodyをクローズします。
-	// これを忘れるとリソースリークの原因になります。
-
-	defer resp.Body.Close() // これで Body は常にクローズされる
-
-formatFile,err := io.ReadAll(resp.Body)
 
 if err != nil {
     // S3オブジェクトの読み取りエラー（ファイルがないなど）
     log.Fatalf("ファイルの内容を読み取れませんでした: %v", err) 
 }
 var mappingData map[string]interface{}
-err = json.Unmarshal(formatFile, &mappingData)
+err = json.Unmarshal(data, &mappingData)
     fmt.Printf("フォーマットファイル: %v\n", mappingData) // fmt.Printf の書式も修正しました
 if err != nil {
     // JSON形式が不正な場合のエラー処理
